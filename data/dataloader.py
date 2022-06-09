00e97fe457262ec5e8cf7731ee070cf610d87599
anuskaroutray <aroutraybbsr01@gmail.com>
2022-06-10 01:26:44 +0530

    implemented VQA dataloader

--- data/dataloader.py
+++ data/dataloader.py
@@ -145,3 +145,141 @@ class ImageCaptionDataset(Dataset):
 		caption = emoji_pattern.sub(r'', caption)
 
 		return caption
+
+class VisualQuestionAnsweringDataset(Dataset):
+
+	def __init__(self, dataset, preprocess_text = True, split = "train", max_length_caption = 64,
+				image_resize = (224, 224), warn_grayscale = False, eval = False):
+
+		self.dataset = dataset
+		self.preprocess_text = preprocess_text
+		self.split = split
+		self.max_length_caption = max_length_caption
+		self.image_resize = image_resize
+		self.warn_grayscale = warn_grayscale
+		
+		self.transform = transforms.Compose([transforms.Resize(self.image_resize), transforms.ToTensor()])
+
+		self.tokenizer = get_tokenizer("basic_english")
+		self.preprocessed_dict = json.load(open(f'./datasets/{self.dataset}/{self.split}_image_captions.json', 'r'))
+		self.vocabulary = torch.load(f'./datasets/{self.dataset}/train_vocabulary.pth')
+		self.concept_vocabulary = torch.load(f'./datasets/{self.dataset}/all_concept_vocabulary.pth')
+
+		self.num_concepts = len(self.concept_vocabulary)
+
+		self.image_ids = []
+		self.image_paths = []
+		self.captions = []
+		self.preprocessed_captions = []
+		self.questions = []
+		self.answers = []
+
+		for image_id, path_caption_dict in self.preprocessed_dict.items():
+			
+			if eval:
+				num_captions = 1
+			else:
+				num_captions = len(path_caption_dict["captions"])
+
+			image_path = path_caption_dict["image_path"]
+			question = path_caption_dict["question"]
+			answer = path_caption_dict["answer"]
+
+			self.image_ids.extend([image_id] * num_captions)   
+			self.image_paths.extend([image_path] * num_captions)
+			self.questions.extend([question] * num_captions)
+			self.answers.extend([answer] * num_captions)
+
+			captions = path_caption_dict['captions'][:num_captions]
+
+			for idx in range(num_captions):
+				if self.preprocess_text:
+					captions[idx] = self.preprocess_caption(captions[idx])
+
+			self.preprocessed_captions.extend(deepcopy(captions))
+
+	def __len__(self):
+		return len(self.image_ids)
+
+	def __getitem__(self, idx):
+
+		image_path = self.image_paths[idx]
+		image = Image.open(image_path)
+
+		# Convert gray scale image to RGB
+		if image.mode == 'L':
+			if self.warn_grayscale:
+				warnings.warn('image %s is grayscale..' % image_path,
+							RuntimeWarning)
+			image = image.convert('RGB')
+		
+		image = self.transform(image)
+
+		caption = self.preprocessed_captions[idx]
+		tokenized_caption = self.tokenizer(caption)
+
+		word_embeddings = self.get_one_hot_word_vector(tokenized_caption, self.max_length_caption)
+		ground_concept_vector = self.get_concept_vector(tokenized_caption)
+
+		question = self.questions[idx]
+		tokenized_question = self.tokenizer(question)
+		question_embeddings = self.get_one_hot_word_vector(tokenized_question, self.max_length_caption)
+
+		answer = self.answers[idx]
+
+		return image, word_embeddings, ground_concept_vector, question_embeddings, answer
+
+	# def get_one_hot_word_vector(self, words, keys: Union[str, Iterable], max_length):
+	def get_one_hot_word_vector(self, words, max_length):
+		
+		if isinstance(words, str):
+			words = [words]
+		
+		one_hot = F.one_hot(torch.tensor(self.vocabulary.lookup_indices(words)), num_classes = len(self.vocabulary))
+		if one_hot.size()[0] > max_length:
+			one_hot = one_hot[:max_length, :]
+		elif one_hot.size()[0] < max_length:
+			padding = torch.zeros((max_length - one_hot.size()[0], one_hot.size()[1]))
+			one_hot = torch.cat([one_hot, padding], dim = 0)
+
+		return one_hot
+
+	def get_concept_vector(self, words):
+
+		concept_vector = torch.zeros((self.num_concepts, 1))
+		for word in words:
+			if self.concept_vocabulary.lookup_indices(word):
+				concept_vector[self.concept_vocabulary.lookup_indices(word)] = 1
+
+		return concept_vector
+
+	def collater(self, items):
+
+		batch = {"image": torch.stack([x[0] for x in items], dim = 0),
+				"word_embeddings": torch.stack([x[1] for x in items], dim = 0),
+				"ground_concept_vector": torch.stack([x[2] for x in items], dim = 0),
+				"question_embeddings": torch.stack([x[3] for x in items], dim = 0),
+				"answer": torch.stack([x[4] for x in items], dim = 0)}
+		return batch
+
+	def preprocess_caption(self, caption):
+
+		caption = re.sub('[^a-zA-Z0-9.?,]', ' ', caption)
+			
+		url = re.compile(r'https?://\S+|www\.\S+')
+		caption = url.sub(r'', caption)
+		
+		html = re.compile(r'<.*?>')
+		caption = html.sub(r'', caption)
+
+		emoji_pattern = re.compile("["
+								u"\U0001F600-\U0001F64F"  # emoticons
+								u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+								u"\U0001F680-\U0001F6FF"  # transport & map symbols
+								u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+								u"\U00002702-\U000027B0"
+								u"\U000024C2-\U0001F251"
+								"]+", flags=re.UNICODE)
+		caption = emoji_pattern.sub(r'', caption)
+
+		return caption
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from utils.env import set_seed
 # from utils.loss import ContrastiveLoss
 from torch.utils.data import DataLoader
-from data.dataloader import ImageCaptionDataset
+from data.dataloader import ImageCaptionDataset, VisualQuestionAnsweringDataset
 # from utils.test import test_retrieval, TestCallback
 from utils.trainer import train_captioning_model, evaluate_captioning_model
 # from src.model import LanguageModel, VisionModel, DualEncoder, SpatialInformationAggregatorModule
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--batch_size', default = 128, type = int)	# 64, 128
-	parser.add_argument('--dataset', default = 'flickr8k', type = str, choices=['coco', 'flickr30k', 'flickr8k', 'toronto-cocoqa'])
+	parser.add_argument('--dataset', default = 'toronto-cocoqa', type = str, choices=['coco', 'flickr30k', 'flickr8k', 'toronto-cocoqa'])
 	parser.add_argument('--max_epochs', default = 10, type = int)
 	parser.add_argument('--dropout', default = 0.4, type = float)
 	parser.add_argument('--eval', default = True, type = bool)
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 	parser.add_argument('--max_length_caption', default = 64, type = int)
 	parser.add_argument('--preprocess_text', default = True, type = bool) # With and without
 	parser.add_argument('--seed', default = 0, type = int)
-	parser.add_argument('--test', default = True, type = bool)
+	parser.add_argument('--test', default = False, type = bool)
 	parser.add_argument('--test_shuffle', default = False, type = bool)
 	parser.add_argument('--train', default = True, type = bool)
 	parser.add_argument('--train_shuffle', default = True, type = bool)
@@ -45,6 +45,9 @@ if __name__ == "__main__":
 	parser.add_argument('--pretrained', default = True, type = bool)
 	parser.add_argument('--bidirectional', default = True, type = bool)
 	parser.add_argument('--epsilon', default = 0.6, type = float)
+	parser.add_argument('--temperature', default = 0.7, type = float)
+	parser.add_argument('--model_type', default = "vqa", type = str, choices = ["vqa", "captioning"])
+	
 
 	args = parser.parse_args()
 
@@ -56,26 +59,56 @@ if __name__ == "__main__":
 		device = torch.device("cuda:0")
 		
 	train_dataloader, validation_dataloader, test_dataloader = None, None, None
-	if args.train:
-		train_data = ImageCaptionDataset(args.dataset, preprocess_text = args.preprocess_text, split = "train", max_length_caption = args.max_length_caption,
-				image_resize = args.image_resize, warn_grayscale = args.warn_grayscale, eval = False)
-		train_dataloader = DataLoader(train_data, batch_size = args.batch_size, shuffle = args.train_shuffle, collate_fn = train_data.collater)
 
-	if args.validation:
-		validation_data = ImageCaptionDataset(args.dataset, preprocess_text = args.preprocess_text, split = "val", max_length_caption = args.max_length_caption,
-				image_resize = args.image_resize, warn_grayscale = args.warn_grayscale, eval = args.eval)
-		validation_dataloader = DataLoader(validation_data, batch_size = args.batch_size, shuffle = args.val_shuffle, collate_fn = validation_data.collater)
-	
-	if args.test:
-		test_data = ImageCaptionDataset(args.dataset, preprocess_text = args.preprocess_text, split = "test", max_length_caption = args.max_length_caption,
-				image_resize = args.image_resize, warn_grayscale = args.warn_grayscale, eval = args.eval)
-		test_dataloader = DataLoader(test_data, batch_size = args.batch_size, shuffle = args.test_shuffle, collate_fn = test_data.collater)
+	if args.model_type == "captioning":
+		if args.train:
+			train_data = ImageCaptionDataset(args.dataset, preprocess_text = args.preprocess_text, split = "train", max_length_caption = args.max_length_caption,
+					image_resize = args.image_resize, warn_grayscale = args.warn_grayscale, eval = False)
+			train_dataloader = DataLoader(train_data, batch_size = args.batch_size, shuffle = args.train_shuffle, collate_fn = train_data.collater)
 
-	vocab_size = len(train_data.vocabulary)
-	model = ImageCaptioningModel(args.model_name, input_size = args.input_size, hidden_size = args.hidden_size, output_size = args.output_size, 
-								pretrained = args.pretrained, bidirectional = args.bidirectional, epsilon = args.epsilon, device = device,
-								vocab_size = vocab_size)
-	model = model.to(device)
+		if args.validation:
+			validation_data = ImageCaptionDataset(args.dataset, preprocess_text = args.preprocess_text, split = "val", max_length_caption = args.max_length_caption,
+					image_resize = args.image_resize, warn_grayscale = args.warn_grayscale, eval = args.eval)
+			validation_dataloader = DataLoader(validation_data, batch_size = args.batch_size, shuffle = args.val_shuffle, collate_fn = validation_data.collater)
+		
+		if args.test:
+			test_data = ImageCaptionDataset(args.dataset, preprocess_text = args.preprocess_text, split = "test", max_length_caption = args.max_length_caption,
+					image_resize = args.image_resize, warn_grayscale = args.warn_grayscale, eval = args.eval)
+			test_dataloader = DataLoader(test_data, batch_size = args.batch_size, shuffle = args.test_shuffle, collate_fn = test_data.collater)
 
-	train_captioning_model(model, train_dataloader, validation_dataloader, args, device = device)
-	evaluate_captioning_model(model, test_dataloader, args, device = device)
+		vocab_size = len(train_data.vocabulary)
+		model = ImageCaptioningModel(args.model_name, input_size = args.input_size, hidden_size = args.hidden_size, output_size = args.output_size, 
+									pretrained = args.pretrained, bidirectional = args.bidirectional, epsilon = args.epsilon, device = device,
+									vocab_size = vocab_size)
+		model = model.to(device)
+
+		train_captioning_model(model, train_dataloader, validation_dataloader, args, device = device)
+		if args.test:
+			evaluate_captioning_model(model, test_dataloader, args, device = device)
+
+	elif args.model_type == "vqa":
+
+		if args.train:
+			train_data = VisualQuestionAnsweringDataset(args.dataset, preprocess_text = args.preprocess_text, split = "train", max_length_caption = args.max_length_caption,
+					image_resize = args.image_resize, warn_grayscale = args.warn_grayscale, eval = False)
+			train_dataloader = DataLoader(train_data, batch_size = args.batch_size, shuffle = args.train_shuffle, collate_fn = train_data.collater)
+
+		if args.validation:
+			validation_data = VisualQuestionAnsweringDataset(args.dataset, preprocess_text = args.preprocess_text, split = "val", max_length_caption = args.max_length_caption,
+					image_resize = args.image_resize, warn_grayscale = args.warn_grayscale, eval = args.eval)
+			validation_dataloader = DataLoader(validation_data, batch_size = args.batch_size, shuffle = args.val_shuffle, collate_fn = validation_data.collater)
+		
+		if args.test:
+			test_data = VisualQuestionAnsweringDataset(args.dataset, preprocess_text = args.preprocess_text, split = "test", max_length_caption = args.max_length_caption,
+					image_resize = args.image_resize, warn_grayscale = args.warn_grayscale, eval = args.eval)
+			test_dataloader = DataLoader(test_data, batch_size = args.batch_size, shuffle = args.test_shuffle, collate_fn = test_data.collater)
+
+		vocab_size = len(train_data.vocabulary)
+		model = ImageCaptioningModel(args.model_name, input_size = args.input_size, hidden_size = args.hidden_size, output_size = args.output_size, 
+									pretrained = args.pretrained, bidirectional = args.bidirectional, epsilon = args.epsilon, device = device,
+									vocab_size = vocab_size)
+		model = model.to(device)
+
+		train_vqa_model(model, train_dataloader, validation_dataloader, args, device = device)
+		if args.test:
+			evaluate_vqa_model(model, test_dataloader, args, device = device)
